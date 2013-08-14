c28d86d34127d3613e337a82df4f917298cb5a8e
Fabian Jakobs <fabian@c9.io>
2013-08-14 10:31:01 +0000

    initial commit

---
+++ immediate.js
@@ -0,0 +1,254 @@
+/**
+ * Immediate Execution Editor for Cloud9 IDE
+ *
+ * @copyright 2010, Ajax.org B.V.
+ * @license GPLv3 <http://www.gnu.org/licenses/gpl.txt>
+ */
+define(function(require, exports, module) {
+    main.consumes = ["editors", "ui", "settings", "tabs", "ace"];
+    main.provides = ["immediate"];
+    return main;
+
+    function main(options, imports, register) {
+        var editors  = imports.editors;
+        // var settings = imports.settings;
+        var tabs     = imports.tabs;
+        var ui       = imports.ui;
+        
+        var Repl     = require("plugins/c9.ide.ace.repl/repl").Repl;
+        var markup   = require("text!./immediate.xml");
+        
+        var counter  = 0;
+        
+        /***** Initialization *****/
+        
+        var extensions = [];
+        
+        var handle = editors.register("immediate", "Immediate Window", 
+                                      Immediate, extensions);
+        var emit   = handle.getEmitter();
+        
+        var replTypes = {}; //Shared across Immediate windows
+        
+        handle.on("load", function(){
+            handle.addElement(
+                tabs.getElement("mnuEditors").appendChild(
+                    new ui.item({
+                        caption : "New Immediate Window",
+                        onclick : function(e){
+                            if (tabs.focussedPage)
+                                e.tab = tabs.focussedPage.tab;
+                            tabs.openEditor("immediate", true, function(){});
+                        }
+                    })
+                )
+            );
+            
+            // Insert some CSS
+            ui.insertCss(require("text!./style.css"), options.staticPrefix, handle);
+        });
+        
+        /**
+         * 
+         */
+        handle.freezePublicAPI({
+            /**
+             * 
+             */
+            addType : function(name, value, evaluator, plugin){
+                if (replTypes[value])
+                    throw new Error("Type is already registered");
+                    
+                replTypes[value] = {
+                    name      : name, 
+                    value     : value, 
+                    evaluator : evaluator,
+                    plugin    : plugin
+                };
+                emit("addType", replTypes[value]);
+                
+                plugin.addOther(function(){ 
+                    emit("removeType", replTypes[value]);
+                    delete replTypes[value]; 
+                });
+            },
+            
+            /**
+             * 
+             */
+            findEvaluator : function(type, callback){
+                if (!type || !replTypes[type]) {
+                    handle.on("addType", function wait(e){
+                        if (!type || e.value == type)
+                            callback(e.value, replTypes[e.value].evaluator);
+                        
+                        handle.off("addType", wait);
+                    });
+                }
+                else {
+                    callback(type, replTypes[type].evaluator);
+                }
+            },
+            
+            /**
+             * 
+             */
+            removeType : function(value){
+                emit("removeType", replTypes[value]);
+                delete replTypes[value];
+            }
+        });
+        
+        function Immediate(){
+            var Baseclass = editors.findEditor("ace");
+            
+            var deps   = main.consumes.splice(0, main.consumes.length - 1);
+            var plugin = new Baseclass(true, [], deps);
+            // var emit   = plugin.getEmitter();
+            
+            var ddType, btnClear, ace;
+            
+            plugin.on("draw", function(e){
+                // Create UI elements
+                ui.insertMarkup(e.page, markup, plugin);
+                
+                ddType    = plugin.getElement("ddType");
+                btnClear  = plugin.getElement("btnClear");
+                
+                ace = plugin.ace;
+                
+                ace.setOption("fontSize", 11);
+                ace.container.style.lineHeight = "17px";
+                
+                e.htmlNode.className += " immediate";
+                
+                ddType.on("afterchange", function(){
+                    if (currentDocument)
+                        currentDocument.getSession().changeType(ddType.value);
+                });
+                btnClear.on("click", function(){
+                    plugin.clear()
+                });
+                
+                for (var type in replTypes){
+                    var t = replTypes[type];
+                    addType(t.name, type, t.plugin);
+                }
+                
+                handle.on("addType", function(e){
+                    addType(e.name, e.value, e.plugin);
+                });
+            })
+            
+            /***** Method *****/
+            
+            function addType(name, value, plugin){
+                var item = ddType.appendChild(new ui.item({
+                    caption : name,
+                    value   : value
+                }));
+                
+                plugin.addElement(item);
+            }
+            
+            // Set the page in loading state - later this could be the output block
+            // currentDocument.page.className.add("loading");
+            // settings.save();
+            
+            /***** Lifecycle *****/
+            
+            plugin.on("load", function(){
+            });
+            
+            var currentDocument;
+            plugin.on("document.load", function(e){
+                var doc     = e.doc;
+                var session = doc.getSession();
+                
+                doc.undoManager.on("change", function(e){
+                    if (!doc.undoManager.isAtBookmark())
+                        doc.undoManager.bookmark();
+                });
+                
+                doc.title = "Immediate";
+                
+                if (session.repl) return;
+                
+                session.changeType = function(type){
+                    handle.findEvaluator(type, function(type, evaluator){
+                        session.type = type;
+                        
+                        if (!session.repl) {
+                            session.repl = new Repl(session.session, {
+                                mode      : evaluator.mode,
+                                evaluator : evaluator,
+                                message   : evaluator.message
+                            });
+                            
+                            if (currentDocument
+                              && currentDocument.getSession() == session)
+                                session.repl.attach(ace);
+                        }
+                        else {
+                            session.repl.setEvaluator(evaluator);
+                            session.repl.setMode(evaluator.mode);
+                        }
+                    });
+                };
+                
+                session.changeType(session.type || ddType.value);
+            });
+            plugin.on("document.activate", function(e){
+                currentDocument = e.doc;
+                var session = e.doc.getSession();
+                
+                if (session.type)
+                    ddType.setValue(session.type);
+                
+                if (session.repl)
+                    session.repl.attach(ace);
+            });
+            plugin.on("document.unload", function(e){
+                var session = e.doc.getSession();
+                if (session.repl)
+                    session.repl.detach();
+                delete session.repl;
+            });
+            plugin.on("state.get", function(e){
+                // @todo at one for each value container
+                e.state.type      = e.doc.getSession().type;
+            });
+            plugin.on("state.set", function(e){
+                if (e.state.type)
+                    ddType.setValue(e.state.type);
+            });
+            plugin.on("clear", function(){
+            });
+            plugin.on("focus", function(){
+            });
+            plugin.on("enable", function(){
+            });
+            plugin.on("disable", function(){
+            });
+            plugin.on("unload", function(){
+            });
+            
+            /***** Register and define API *****/
+            
+            /**
+             * Read Only Image Editor
+             **/
+            plugin.freezePublicAPI({
+                
+            });
+            
+            plugin.load("immediate" + counter++);
+            
+            return plugin;
+        }
+        
+        register(null, {
+            immediate: handle
+        });
+    }
+});
---
+++ immediate_test.js
@@ -0,0 +1,167 @@
+/*global describe it before after  =*/
+
+require(["lib/architect/architect", "lib/chai/chai", "/vfs-root"], 
+  function (architect, chai, baseProc) {
+    var expect = chai.expect;
+    
+    var runners  = {
+        "node" : {
+            "caption" : "Node.js (current)",
+            "cmd": ["node", "$file"],
+            "debug": ["--debug-brk=5454"],
+            "file_regex": "^[ ]*File \"(...*?)\", line ([0-9]*)",
+            "selector": "source.python",
+            "info": "Your code is running at \\033[01;34m$hostname\\033[00m.\n"
+                + "\\033[01;31mImportant:\\033[00m use \\033[01;32mprocess.env.PORT\\033[00m as the port and \\033[01;32mprocess.env.IP\\033[00m as the host in your scripts!\n"
+        },
+        "pythoni" : {
+            "caption" : "Python in interactive mode",
+            "cmd": ["python", "-i"],
+            "selector": "source.python",
+            "info": "Hit \\033[01;34mCtrl-D\\033[00m to exit.\n"
+        }
+    };
+    
+    architect.resolveConfig([
+        {
+            packagePath : "plugins/c9.core/c9",
+            workspaceId : "ubuntu/ip-10-35-77-180",
+            startdate   : new Date(),
+            debug       : true,
+            smithIo     : "{\"prefix\":\"/smith.io/server\"}",
+            hosted      : true,
+            local       : false,
+            hostname    : "dev.javruben.c9.io",
+            davPrefix   : "/"
+        },
+        
+        "plugins/c9.core/ext",
+        "plugins/c9.core/events",
+        "plugins/c9.core/http",
+        "plugins/c9.core/util",
+        "plugins/c9.ide.ui/anims",
+        "plugins/c9.ide.ui/lib_apf",
+        {
+            packagePath : "plugins/c9.core/settings",
+            settings : "<settings><state><console>" + JSON.stringify({
+                type  : "tab", 
+                nodes : [
+                    {
+                        type : "page",
+                        editorType : "immediate",
+                        active : "true"
+                    }
+                ]
+            }) + "</console></state></settings>"
+        },
+        {
+            packagePath  : "plugins/c9.ide.ui/ui",
+            staticPrefix : "plugins/c9.ide.ui"
+        },
+        "plugins/c9.ide.editors/document",
+        "plugins/c9.ide.editors/undomanager",
+        "plugins/c9.ide.editors/editors",
+        "plugins/c9.ide.editors/editor",
+        {
+            packagePath : "plugins/c9.ide.editors/tabs",
+            testing     : 2
+        },
+        "plugins/c9.ide.editors/tab",
+        "plugins/c9.ide.editors/page",
+        "plugins/c9.ide.ace/ace",
+        {
+            packagePath: "plugins/c9.ide.immediate/immediate",
+            staticPrefix : "plugins/c9.ide.layout.classic"
+        },
+        "plugins/c9.ide.immediate/evaluators/js-current-browser",
+        "plugins/c9.ide.console/console",
+        "plugins/c9.fs/proc",
+        "plugins/c9.fs/fs",
+        "plugins/c9.fs/vfs",
+        "plugins/c9.ide.keys/commands",
+        {
+            packagePath : "plugins/c9.ide.run/run",
+            testing     : true,
+            base        : baseProc,
+            runners     : runners
+        },
+        
+        // Mock plugins
+        {
+            consumes : ["emitter", "apf", "ui"],
+            provides : [
+                "menus", "layout", "watcher", 
+                "save", "fs", "preferences", "clipboard"
+            ],
+            setup    : expect.html.mocked
+        },
+        {
+            consumes : ["immediate", "tabs"],
+            provides : [],
+            setup    : main
+        }
+    ], function (err, config) {
+        if (err) throw err;
+        var app = architect.createApp(config);
+        app.on("service", function(name, plugin){ plugin.name = name; });
+    });
+    
+    function main(options, imports, register) {
+        var immediate = imports.immediate;
+        var tabs      = imports.tabs;
+        
+        expect.html.setConstructor(function(page){
+            if (typeof page == "object")
+                return page.tab.aml.getPage("editor::" + page.editorType).$ext;
+        });
+        
+        function countEvents(count, expected, done){
+            if (count == expected) 
+                done();
+            else
+                throw new Error("Wrong Event Count: "
+                    + count + " of " + expected);
+        }
+        
+        describe('immediate', function() {
+            before(function(done){
+                apf.config.setProperty("allow-select", false);
+                apf.config.setProperty("allow-blur", false);
+
+                bar.$ext.style.background = "rgba(220, 220, 220, 0.93)";
+                bar.$ext.style.position = "fixed";
+                bar.$ext.style.left = "20px";
+                bar.$ext.style.right = "20px";
+                bar.$ext.style.bottom = "20px";
+                bar.$ext.style.height = "150px";
+      
+                document.body.style.marginBottom = "150px";
+                
+               // for (var name in runners) {
+               //     immediate.addType(name.uCaseFirst(), name);
+               // }
+                
+                done();
+            });
+            
+            describe("addType(), on.evaluate", function(){
+                it('should add an item to the dropdown and run it', function(done) {
+                    // var inp = tabs.focussedPage.editor.getElement("txtInput");
+                    // inp.setValue("1+1");
+                    // inp.dispatchEvent("keydown", {keyCode: 13});
+                    done();
+                });
+            });
+            
+            // after(function(done){
+            //     tabs.unload();
+            //     bar.parentNode.removeChild(bar);
+            //     
+            //     document.body.style.marginBottom = "";
+            //     done();
+            // });
+        });
+        
+        onload && onload();
+    }
+});
