fcc41b339569a596517a651ca0382709279ecae0
Mostafa Eweda <mostafa@c9.io>
2013-12-17 16:17:15 +0100

    Fix remainder api-dependant tests

--- immediate_test.js
+++ immediate_test.js
@@ -54,6 +54,7 @@ require(["lib/architect/architect", "lib/chai/chai"],
                 ]
             }) + "</console></state></settings>"
         },
+        "plugins/c9.ide.api/api.js",
         {
             packagePath  : "plugins/c9.ide.ui/ui",
             staticPrefix : "plugins/c9.ide.ui"
